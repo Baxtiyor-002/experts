bedfb8807485973aea3bd090ccff019f861e4a54
Baxtiyor Samugov <baxtiyorsamugov@gmail.com>
2023-02-13 12:16:53 +0500

    Admin panel customized

--- experts/admin.py
+++ experts/admin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Type, Sphere, Sex, Structure, Experts, Organization, Country
 # Register your models here.
+
+
+@admin.register(Experts)
+class ExpertsAdmin(admin.ModelAdmin):
+    list_display = ("id", "fname", "name", "type", "sphere", "country", "get_image")
+    list_display_links = ("fname", "name", "type")
+    list_filter = ("type", "sphere", "sex")
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.photo.url} width="100" height="auto"')
+
+    get_image.short_description = "Изображение"
+
 
 admin.site.register(Type)
 admin.site.register(Sphere)
 admin.site.register(Sex)
 admin.site.register(Structure)
-admin.site.register(Experts)
 admin.site.register(Organization)
 admin.site.register(Country)
